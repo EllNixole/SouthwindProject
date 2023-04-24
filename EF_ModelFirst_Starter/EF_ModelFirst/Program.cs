8a75a0346e3f98fd6b6d39c9132ed6d4727af014
DanielleMassey <danimassey1998@gmail.com>
2023-04-24 16:23:22 +0100

    added modelfirst solution

---
+++ EF_ModelFirst_Starter/EF_ModelFirst/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EF_ModelFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SouthwindContext())
+            {
+                db.Customers.Add(new Customer() { ContactName = "Philip Thomas", City = "IronBridge", CustomerId = "PHILT", PostalCode = "AB1 2CD" });
+                db.Customers.Add(new Customer() { ContactName = "Danyal Saleh", City = "Reading", CustomerId = "DANYS", PostalCode = "AB1 2CB" });
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
