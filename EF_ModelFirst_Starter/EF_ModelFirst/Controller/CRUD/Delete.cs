cdbd3e9eb62d8ca1b420c86c9c1488a0d4daf98c
HumidTurtle <nooreenali999@gmail.com>
2023-04-25 13:42:10 +0100

    updates

--- EF_ModelFirst_Starter/EF_ModelFirst/Controller/CRUD/Delete.cs
+++ EF_ModelFirst_Starter/EF_ModelFirst/Controller/CRUD/Delete.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace EF_ModelFirst;
 
@@ -8,8 +9,22 @@ public class Delete : DataAction
     {
         using (var db = new SouthwindContext())
         {
+            List<Order> orderToDelete = new List<Order> { db.Orders.Find(customerID) };
+            List<OrderDetail> orderDetailToDelete = new List<OrderDetail> { db.OrderDetails. };
+            foreach (var orders in orderToDelete)
+            {
+                orderDetailToDelete = new List<OrderDetail> { };
+                orderDetailToDelete.Add(db.OrderDetails.Find(orders.OrderId));
+                foreach (var orderDetails in orderDetailToDelete)
+                {
+                    db.OrderDetails.Remove(db.OrderDetails.Find(orderDetails.OrderId));
+                }                
+                db.Orders.Remove(db.Orders.Find(orders.OrderId));                
+            }
+
             var customerToDelete = db.Customers.Find(customerID);
             db.Customers.Remove(customerToDelete);
+
             db.SaveChanges();
 
         }
